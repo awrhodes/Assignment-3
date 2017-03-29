b4ce6d9edfc2fdae2369e2b451e93de2facba6bb
Alex <Alex Rhodes>
2017-03-29 19:24:24 -0400

    Updated

---
+++ source/nav.py
@@ -0,0 +1,71 @@
+import rospy
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from math import radians, degrees
+from actionlib_msgs.msgmsg import *
+from geometry_msgs.msgmsg import Point
+
+class Navigation():
+    def __init__(self):
+        #goal1 is the corner of the room with the workstations
+        #The robot will then vist the other corners of the room clockwise
+        self.goal1_x = 2.73
+        self.goal1_y = 2.10
+        self.goal2_x = 2.47
+        self.goal2_y = -1.47
+        self.goal3_x = -6.15
+        self.goal3_y = -0.37
+        self.goal4_x = -5.55
+        self.goal4_y = 4.14
+
+        rospy.init_node('Navigation')
+
+        self.cornerReached = goToCorner(self.goal1_x, self.goal1_y)
+        if self.cornerReached():
+            print("Corner 1 reached.")
+        else:
+            print("Failed to reach Corner 1.")
+
+        self.cornerReached = goToCorner(self.goal2_x, self.goal2_y)
+        if self.cornerReached():
+            print("Corner 2 reached.")
+        else:
+            print("Failed to reach Corner 2.")
+
+        self.cornerReached = goToCorner(self.goal3_x, self.goal3_y)
+        if self.cornerReached():
+            print("Corner 3 reached.")
+        else:
+            print("Failed to reach Corner 3.")
+
+        self.cornerReached = goToCorner(self.goal4_x, self.goal4_y)
+        if self.cornerReached():
+            print("Corner 4 reached.")
+        else:
+            print("Failed to reach corner 4.")
+
+    def goToCorner(self, x, y):
+        actionlib = actionlib.SimpleActionClient("moveBot", MoveBaseAction)
+
+        corner = MoveBaseGoal()
+
+        corner.target_pose.header.frame_id = "map"
+        corner.target_pose.header.stamp = rospy.Time.now()
+
+        corner.target_pose.position = Point(x, y, 0)
+
+        actionlib.send_goal(corner)
+
+        if actionlib.get_state() == GoalStatus.SUCCEEDED:
+            print("Corner reached")
+            return True
+        else:
+            print("Failed to reach corner.")
+            return False
+
+if __name__ == 'main':
+    try:
+        Navigation()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        print("ROSInterruptException. Navigation failed.")
