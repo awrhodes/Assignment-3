62cdd0d5c04429f786e2a4e73b467d2426453cf0
Alex <Alex Rhodes>
2017-03-30 13:53:51 -0400

    Syntax

--- src/nav.py
+++ src/nav.py
@@ -2,8 +2,8 @@ import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from math import radians, degrees
-from actionlib_msgs.msgmsg import *
-from geometry_msgs.msgmsg import Point
+from actionlib_msgs.msg import *
+from geometry_msgs.msg import Point
 
 class Navigation():
     def __init__(self):
